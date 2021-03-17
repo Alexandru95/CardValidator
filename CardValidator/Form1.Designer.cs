52a7d23245786c82561fda466ef33410c5b8d28c
Alexandru Tana <Alexandru.Tana@channelsight.com>
2021-03-17 17:10:37 +0200

    Simple WindowsForms Card Validator

---
+++ CardValidator/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace CardValidator
+{
+    partial class CardValidator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CardNumberText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ExpiryDateText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CVVText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Card Number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CardNumberText
+            // 
+            this.CardNumberText.Location = new System.Drawing.Point(128, 48);
+            this.CardNumberText.Margin = new System.Windows.Forms.Padding(2);
+            this.CardNumberText.Name = "CardNumberText";
+            this.CardNumberText.Size = new System.Drawing.Size(241, 20);
+            this.CardNumberText.TabIndex = 1;
+            this.CardNumberText.TextChanged += new System.EventHandler(this.CardNumberText_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Expiry Date";
+            // 
+            // ExpiryDateText
+            // 
+            this.ExpiryDateText.Location = new System.Drawing.Point(128, 86);
+            this.ExpiryDateText.Margin = new System.Windows.Forms.Padding(2);
+            this.ExpiryDateText.Name = "ExpiryDateText";
+            this.ExpiryDateText.Size = new System.Drawing.Size(76, 20);
+            this.ExpiryDateText.TabIndex = 3;
+            this.ExpiryDateText.TextChanged += new System.EventHandler(this.ExpiryDateText_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(272, 90);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CVV";
+            // 
+            // CVVText
+            // 
+            this.CVVText.Location = new System.Drawing.Point(305, 88);
+            this.CVVText.Margin = new System.Windows.Forms.Padding(2);
+            this.CVVText.Name = "CVVText";
+            this.CVVText.Size = new System.Drawing.Size(63, 20);
+            this.CVVText.TabIndex = 5;
+            this.CVVText.TextChanged += new System.EventHandler(this.CVVText_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(467, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 19);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Validate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CardValidator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 131);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CVVText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ExpiryDateText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CardNumberText);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "CardValidator";
+            this.Text = "Validate your card";
+            this.Load += new System.EventHandler(this.CardValidator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CardNumberText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ExpiryDateText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox CVVText;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CardValidator/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardValidator
+{
+    public partial class CardValidator : Form
+    {
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CsvCode { get; set; }
+        public string ValidationError { get; set; }
+        public CardValidator()
+        {
+            InitializeComponent();
+        }
+
+        private void CardValidator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ValidationError = IsCreditCardInfoValid(CardNumber, ExpirationDate, CsvCode);
+            if (ValidationError.Contains("is incorrect"))
+            {
+                MessageBox.Show($"{ValidationError}", "Validation Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (ValidationError == "")
+            {
+                MessageBox.Show("The values are correct", "Validation Complete",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CardValidator form = new CardValidator();
+                form.Refresh();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CardNumberText_TextChanged(object sender, EventArgs e)
+        {
+            CardNumber = CardNumberText.Text;
+        }
+        private void ExpiryDateText_TextChanged(object sender, EventArgs e)
+        {
+            ExpirationDate = ExpiryDateText.Text;
+        }
+
+        private void CVVText_TextChanged(object sender, EventArgs e)
+        {
+            CsvCode = CVVText.Text;
+        }
+
+        public static string IsCreditCardInfoValid(string cardNo, string expiryDate, string cvv)
+        {
+            var cardCheck = new Regex(@"^(1298|1267|4512|4567|8901|8933)([\-\s]?[0-9]{4}){3}$");
+            var monthCheck = new Regex(@"^(0[1-9]|1[0-2])$");
+            var yearCheck = new Regex(@"^20[0-9]{2}$");
+            var cvvCheck = new Regex(@"^\d{3}$");
+            var dateParts = expiryDate.Split('/'); //expiry date in from MM/yyyy
+            var year = int.Parse(dateParts[1]);
+            var month = int.Parse(dateParts[0]);
+            var lastDateOfExpiryMonth = DateTime.DaysInMonth(year, month); //get actual expiry date
+            var cardExpiry = new DateTime(year, month, lastDateOfExpiryMonth, 23, 59, 59);
+            var validationError = "";
+
+            if (!cardCheck.IsMatch(cardNo)) // <1>check card number is valid
+            {
+
+                return validationError = "Card number is incorrect";
+
+            }
+            if (!cvvCheck.IsMatch(cvv))// <2>check cvv is valid as "999"
+            {
+
+                return validationError = "CVV number is incorrect";
+            }
+            if (!monthCheck.IsMatch(dateParts[0]) || !yearCheck.IsMatch(dateParts[1])) // <3 - 6> // ^ check date format is valid as "MM/yyyy"
+            {
+
+                return validationError = "Date format is incorrect";
+            }
+            if (!(cardExpiry > DateTime.Now && cardExpiry < DateTime.Now.AddYears(6)))  //check expiry greater than today & within next 6 years <7, 8>>
+            {
+
+                return validationError = "Expiry Date is incorrect";
+            }
+
+            return validationError;
+
+        }
+
+    }
+}
